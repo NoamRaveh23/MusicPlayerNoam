060b955f508275f3b3ffbb31b7eac9333f497521
StudentC <StudentC@CYBERN-13>
2021-11-17 14:42:02 +0200

    Add project files.

---
+++ MusicPlayerNoam/MainActivity.cs
@@ -0,0 +1,79 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Android.Content;
+using Android.Media;
+
+
+namespace MusicPlayerNoam
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btp, bts , btpause , btpiano;
+        TextView tv;
+        SeekBar sb;
+        MediaPlayer mp;
+        AudioManager am;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            btp = (Button)FindViewById(Resource.Id.bt1);
+            bts = (Button)FindViewById(Resource.Id.bt2);
+            btpause = (Button)FindViewById(Resource.Id.bt3);
+            btpiano = (Button)FindViewById(Resource.Id.btpiano);
+            sb = (SeekBar)FindViewById(Resource.Id.sb);
+            tv = (TextView)FindViewById(Resource.Id.tv);
+            mp = MediaPlayer.Create(this, Resource.Raw.BenBassatSong);
+            mp.Start();
+            am = (AudioManager)GetSystemService(Context.AudioService);
+            int max = am.GetStreamMaxVolume(Stream.Music);
+            sb.Max = max;
+            am.SetStreamVolume(Stream.Music, max / 2, 0);
+            sb.ProgressChanged += Sb_ProgressChanged; ;
+            btp.Click += Btp_Click;
+            bts.Click += Bts_Click;
+            btpause.Click += Btpause_Click;
+            btpiano.Click += Btpiano_Click;
+        }
+
+        private void Btpiano_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(PianoActivity));
+            StartActivity(intent);
+        }
+
+        private void Btpause_Click(object sender, System.EventArgs e)
+        {
+            mp.Pause();
+        }
+
+        private void Bts_Click(object sender, System.EventArgs e)
+        {
+            mp.Stop();
+        }
+
+        private void Btp_Click(object sender, System.EventArgs e)
+        {
+            mp.Start();
+
+        }
+
+        private void Sb_ProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
+        {
+            am.SetStreamVolume(Stream.Music, e.Progress, VolumeNotificationFlags.PlaySound);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ MusicPlayerNoam/PianoActivity.cs
@@ -0,0 +1,87 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MusicPlayerNoam
+{
+    [Activity(Label = "PianoActivity")]
+    public class PianoActivity : Activity
+    {
+        Button btdo, re, mi, fa, sol, la, ci;
+        MediaPlayer mp;
+        AudioManager am;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.PianoLayout);
+            btdo = (Button)FindViewById(Resource.Id.btdo);
+            re = (Button)FindViewById(Resource.Id.btre);
+            mi = (Button)FindViewById(Resource.Id.btmi);
+            fa = (Button)FindViewById(Resource.Id.btfa);
+            sol = (Button)FindViewById(Resource.Id.btso);
+            la = (Button)FindViewById(Resource.Id.btla);
+            ci = (Button)FindViewById(Resource.Id.btci);
+            btdo.Click += Btdo_Click;
+            re.Click += Re_Click;
+            mi.Click += Mi_Click;
+            fa.Click += Fa_Click;
+            sol.Click += Sol_Click;
+            la.Click += La_Click;
+            ci.Click += Ci_Click;
+            am = (AudioManager)GetSystemService(Context.AudioService);
+            int max = am.GetStreamMaxVolume(Stream.Music);
+            am.SetStreamVolume(Stream.Music, max / 2, 0);
+        }
+
+        private void Ci_Click(object sender, EventArgs e)
+        {
+            mp = MediaPlayer.Create(this, Resource.Raw.ci);
+            mp.Start();
+        }
+
+        private void La_Click(object sender, EventArgs e)
+        {
+            mp = MediaPlayer.Create(this, Resource.Raw.la);
+            mp.Start();
+        }
+
+        private void Sol_Click(object sender, EventArgs e)
+        {
+            mp = MediaPlayer.Create(this, Resource.Raw.so);
+            mp.Start();
+        }
+
+        private void Fa_Click(object sender, EventArgs e)
+        {
+            mp = MediaPlayer.Create(this, Resource.Raw.fa);
+            mp.Start();
+        }
+
+        private void Mi_Click(object sender, EventArgs e)
+        {
+            mp = MediaPlayer.Create(this, Resource.Raw.mi);
+            mp.Start();
+        }
+
+        private void Re_Click(object sender, EventArgs e)
+        {
+            mp = MediaPlayer.Create(this, Resource.Raw.re);
+            mp.Start();
+        }
+
+        private void Btdo_Click(object sender, EventArgs e)
+        {
+            mp = MediaPlayer.Create(this, Resource.Raw.ddo);
+            mp.Start();
+        }
+    }
+}
